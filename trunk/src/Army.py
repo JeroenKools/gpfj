715d00197b98b01b334570daff649c04fc0aa2ea
koolsjeroen@gmail.com <koolsjeroen@gmail.com@0ad6b3d0-ee24-fc04-b20e-4b017ed9cc3c>
2012-06-28 03:18:44 +0000

    version 0.2 reimplemented Army and Unit classes deal with clicks in side panel

--- trunk/src/Army.py
+++ trunk/src/Army.py
@@ -1,50 +1,159 @@
+from constants import *
 from Tkinter import PhotoImage
 
-ICON_DIR = 'uniticons'
-ICON_TYPE = 'gif' 
 
-class Army:  
-    ranks = ['marshal', 'general', 'colonel', 'major', 'captain', 'lieutenant', 'sergeant', 'miner', 'scout', 'spy', 'bomb', 'flag']
-    
-    def __init__(self, size = 40):
-        
-        # Army as dictionary: name is key, entry contains rank, alive, locations. Negative Locations are on the side panel (dead or to be placed)          
-        self.army = { 
-                     'marshal':    [ 10, [1], [None] ],\
-                      'general':    [ 10, [1], [None] ],\
-                      'colonel':    [ 10, [1 ,1], [None, None] ],\
-                      'major' :     [ 10, [1, 1, 1], [None, None, None] ],\
-                      'captain':    [ 10, [1, 1, 1, 1], [None, None, None, None] ],\
-                      'lieutenant': [ 10, [1, 1, 1, 1], [None, None, None, None] ],\
-                      'sergeant':   [ 10, [1, 1, 1, 1], [None, None, None, None] ],\
-                      'miner':      [ 10, [1, 1, 1, 1, 1], [None, None, None, None, None] ],\
-                      'scout':      [ 10, [1, 1, 1, 1, 1, 1, 1, 1], [None, None, None, None, None, None, None, None] ],\
-                      'spy':        [ 10, [1], [None] ],\
-                      'bomb':       [ 10, [1, 1, 1, 1, 1, 1], [None, None, None, None, None, None] ],\
-                      'flag':       [ 10, [1], [None] ]
-                    }
-        
-        self.icons = {
-                      'marshal':    PhotoImage(file="%s/marshal.%s" % (ICON_DIR, ICON_TYPE)), \
-                      'general':    PhotoImage(file="%s/general.%s" % (ICON_DIR, ICON_TYPE)), \
-                      'colonel':    PhotoImage(file="%s/colonel.%s" % (ICON_DIR, ICON_TYPE)), \
-                      'major':      PhotoImage(file="%s/major.%s" % (ICON_DIR, ICON_TYPE)), \
-                      'captain':    PhotoImage(file="%s/captain.%s" % (ICON_DIR, ICON_TYPE)), \
-                      'lieutenant': PhotoImage(file="%s/lieutenant.%s" % (ICON_DIR, ICON_TYPE)), \
-                      'sergeant':   PhotoImage(file="%s/sergeant.%s" % (ICON_DIR, ICON_TYPE)), \
-                      'miner':      PhotoImage(file="%s/miner.%s" % (ICON_DIR, ICON_TYPE)), \
-                      'scout':      PhotoImage(file="%s/scout.%s" % (ICON_DIR, ICON_TYPE)), \
-                      'spy':        PhotoImage(file="%s/spy.%s" % (ICON_DIR, ICON_TYPE)), \
-                      'bomb':       PhotoImage(file="%s/bomb.%s" % (ICON_DIR, ICON_TYPE)), \
-                      'flag':       PhotoImage(file="%s/flag.%s" % (ICON_DIR, ICON_TYPE))
-                    }
-    def offBoard(self, rank, nr):
-        position = self.army[rank][2][nr] 
-        if position == None: return True
-        (x,y) = position
-        if x < 0 and y < 0: return True
-        return False
-    
-    def changePosition(self, rank, nr, x, y):
-        self.army[rank][2][nr] = (x,y)
-             
+class Army:
+    ranks = ['marshal', 'general', 'colonel', 'major', 'captain',
+             'lieutenant', 'sergeant', 'miner', 'scout', 'spy', 'bomb', 'flag']
+
+    def __init__(self):
+        """Represents a Stratego army as a list of Units."""
+
+        self.army = [
+                      Marshal(),
+                      General(),
+                      Colonel(), Colonel(),
+                      Major(), Major(), Major(),
+                      Captain(), Captain(), Captain(), Captain(),
+                      Lieutenant(), Lieutenant(), Lieutenant(), Lieutenant(),
+                      Sergeant(), Sergeant(), Sergeant(), Sergeant(),
+                      Miner(), Miner(), Miner(), Miner(), Miner(),
+                      Scout(), Scout(), Scout(), Scout(), Scout(), Scout(), Scout(), Scout(),
+                      Spy(),
+                      Bomb(), Bomb(), Bomb(), Bomb(), Bomb(), Bomb(),
+                      Flag()
+        ]
+
+    def getUnit(self, x, y):
+        for unit in self.army:
+            if unit.getPosition() == (x, y):
+                return unit
+
+class Unit:
+    def __init__(self, position=None):
+        self.position = position
+        self.alive = True
+        self.walkFar = False        # scout ability
+        self.killMarshall = False    # spy ability
+
+    def getPosition(self):
+        return self.position
+
+    def isOffBoard(self):
+        if self.position is None:
+            return True
+        else: return False
+
+    def setPosition(self, x, y):
+        self.position = (x, y)
+
+    def die(self):
+        self.alive = False
+        self.position = None
+
+    def getIcon(self):
+        return self.icon
+
+    def __str__(self):
+        if self.position:
+            return "%s at %s" % (self.name, self.position)
+        else:
+            return "Off-board %s" % (self.type)
+
+class Marshal(Unit):
+    def __init__(self, position=None):
+        Unit.__init__(self, position)
+        self.moving = True
+        self.rank = 10
+        self.name = "Marshal"
+        self.icon = PhotoImage(file="%s/%s.%s" % (ICON_DIR, self.name, ICON_TYPE))
+
+class General(Unit):
+    def __init__(self, position=None):
+        Unit.__init__(self, position)
+        self.moving = True
+        self.rank = 9
+        self.name = "General"
+        self.icon = PhotoImage(file="%s/%s.%s" % (ICON_DIR, self.name, ICON_TYPE))
+
+class Colonel(Unit):
+    def __init__(self, position=None):
+        Unit.__init__(self, position)
+        self.moving = True
+        self.rank = 8
+        self.name = "Colonel"
+        self.icon = PhotoImage(file="%s/%s.%s" % (ICON_DIR, self.name, ICON_TYPE))
+
+class Major(Unit):
+    def __init__(self, position=None):
+        Unit.__init__(self, position)
+        self.moving = True
+        self.rank = 7
+        self.name = "Major"
+        self.icon = PhotoImage(file="%s/%s.%s" % (ICON_DIR, self.name, ICON_TYPE))
+
+class Captain(Unit):
+    def __init__(self, position=None):
+        Unit.__init__(self, position)
+        self.moving = True
+        self.rank = 6
+        self.name = "Captain"
+        self.icon = PhotoImage(file="%s/%s.%s" % (ICON_DIR, self.name, ICON_TYPE))
+
+class Lieutenant(Unit):
+    def __init__(self, position=None):
+        Unit.__init__(self, position)
+        self.moving = True
+        self.rank = 5
+        self.name = "Lieutenant"
+        self.icon = PhotoImage(file="%s/%s.%s" % (ICON_DIR, self.name, ICON_TYPE))
+
+class Sergeant(Unit):
+    def __init__(self, position=None):
+        Unit.__init__(self, position)
+        self.moving = True
+        self.rank = 4
+        self.name = "Sergeant"
+        self.icon = PhotoImage(file="%s/%s.%s" % (ICON_DIR, self.name, ICON_TYPE))
+
+class Miner(Unit):
+    def __init__(self, position=None):
+        Unit.__init__(self, position)
+        self.moving = True
+        self.rank = 3
+        self.name = "Miner"
+        self.icon = PhotoImage(file="%s/%s.%s" % (ICON_DIR, self.name, ICON_TYPE))
+
+class Scout(Unit):
+    def __init__(self, position=None):
+        Unit.__init__(self, position)
+        self.moving = True
+        self.moveFar = True # special scout ability!
+        self.rank = 2
+        self.name = "Scout"
+        self.icon = PhotoImage(file="%s/%s.%s" % (ICON_DIR, self.name, ICON_TYPE))
+
+class Spy(Unit):
+    def __init__(self, position=None):
+        Unit.__init__(self, position)
+        self.moving = True
+        self.killMarshall = True
+        self.rank = 1
+        self.name = "Spy"
+        self.icon = PhotoImage(file="%s/%s.%s" % (ICON_DIR, self.name, ICON_TYPE))
+
+class Bomb(Unit):
+    def __init__(self, position=None):
+        Unit.__init__(self, position)
+        self.moving = False
+        self.rank = 99
+        self.name = "Bomb"
+        self.icon = PhotoImage(file="%s/%s.%s" % (ICON_DIR, self.name, ICON_TYPE))
+
+class Flag(Unit):
+    def __init__(self, position=None):
+        Unit.__init__(self, position)
+        self.moving = False
+        self.rank = 0
+        self.name = "Flag"
+        self.icon = PhotoImage(file="%s/%s.%s" % (ICON_DIR, self.name, ICON_TYPE))
