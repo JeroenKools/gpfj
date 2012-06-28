eb683f6443d3f60af144cb8769a6c2a82826affb
koolsjeroen <koolsjeroen@gmail.com>
2012-06-28 03:18:44 +0000

    version 0.2 reimplemented Army and Unit classes deal with clicks in side panel

---
+++ src/constants.py
@@ -0,0 +1,17 @@
+'''
+Constants module for Stratego
+Created on 28 jun. 2012
+
+@author: Jeroen Kools and Fedde Burgers
+'''
+
+GAME_NAME = 'Stratego'
+ICON_DIR = 'uniticons'
+ICON_TYPE = 'gif'
+VERSION = 0.2               # Game version
+GRASS_COLOR = "#44AA22"     # Grass color rgb
+WATER_COLOR = "#3388AA"     # Water color rgb
+UNIT_PANEL_COLOR = "#999999"# Unit panel rgb
+BOARD_WIDTH = 16            # Board size in tiles, default is 10 
+TILE_PIX = 30               # Tile size in pixels
+POOLS = 2                   # Number of pools, default is 2
