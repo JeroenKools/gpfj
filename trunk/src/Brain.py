0fc67edeac0ce10054c0c5c27080b7440421fa77
koolsjeroen@gmail.com <koolsjeroen@gmail.com@0ad6b3d0-ee24-fc04-b20e-4b017ed9cc3c>
2012-06-29 03:30:03 +0000

    Version 0.26 - Added unit movement, placement and highlighting - Added transparenct background to unit icon images - Added Army color drawing on units - Added AI brain stub classes - Improved unit drawing method

---
+++ trunk/src/Brain.py
@@ -0,0 +1,15 @@
+'''
+Created on 29 jun. 2012
+
+@author: Jeroen Kools
+'''
+
+class Brain:
+    def __init__(self):
+        pass
+
+    def placeArmy(self):
+        pass
+
+    def doMove(self):
+        pass
--- trunk/src/constants.py
+++ trunk/src/constants.py
@@ -5,13 +5,16 @@ Created on 28 jun. 2012
 @author: Jeroen Kools and Fedde Burgers
 '''
 
-GAME_NAME = 'Stratego'
+GAME_NAME = 'Stratego'      # Kindral? Praetor?
 ICON_DIR = 'uniticons'
 ICON_TYPE = 'gif'
-VERSION = 0.2               # Game version
+VERSION = 0.26              # Game version
 GRASS_COLOR = "#44AA22"     # Grass color rgb
 WATER_COLOR = "#3388AA"     # Water color rgb
-UNIT_PANEL_COLOR = "#999999"# Unit panel rgb
-BOARD_WIDTH = 16            # Board size in tiles, default is 10 
+UNIT_PANEL_COLOR = "#BBB"   # Unit panel rgb
+RED_PLAYER_COLOR = "#A00"
+SELECTED_RED_PLAYER_COLOR = "#e00"
+BLUE_PLAYER_COLOR = "#00A"
+BOARD_WIDTH = 10            # Board size in tiles, classic Stratego value is 10 
 TILE_PIX = 30               # Tile size in pixels
-POOLS = 2                   # Number of pools, default is 2
+POOLS = 2                   # Number of pools, classic Stratego value is 2
